6dc6b14a2b514f7b1414ea0eb75e13d4865d7db6
ADILAHAMEDK <adilahamedk5842@gmail.com>
2026-01-13 16:43:23 +0530

    filter

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -23,7 +23,7 @@ const Doctors = () => {
   return (
     <div>
       <p className='text-gray-600'>Browse through the doctors specialist.</p>
-      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5 overflow-hidden'>
         <div className='flex flex-col gap-4 text-sm text-gray-600'>
           <p onClick={()=> speciality === "General physician" ? navigate('/doctors') : navigate("/doctors/General physician")} className={`w-[94vh] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
           <p onClick={()=> speciality === "Gynecologist" ? navigate('/doctors') : navigate("/doctors/Gynecologist")} className={`w-[94vh] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
